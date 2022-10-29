5542dfd9d2cb79c447af5806c423fd49fb396a32
keogami <41939011+keogami@users.noreply.github.com>
2022-10-29 21:32:49 +0530

    handles /gitwatch command

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,6 +1,38 @@
-const data = {
-  myVar: Deno.env.get("MY_VAR"),
-  url: Deno.env.get("DEPLOY_URL"),
+const TELEGRAM_TOKEN = Deno.env.get("TELEGRAM_TOKEN")
+const TELEGRAM_URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`
+const methodUrl = (name: String) => `${TELEGRAM_URL}/${name}`
+
+const call = (method: string) => (body: FormData) => fetch(
+  methodUrl(method), {
+    method: "POST",
+    body: body,
+  }
+)
+
+const sendMessage = call("sendMessage")
+
+const sendMessageArg = (chatID: Number, text: string) => {
+  const fd = new FormData()
+  fd.set("chat_id", chatID.toString())
+  fd.set("text", text)
+  return fd
 }
 
-export default async (_req: Request) => new Response(JSON.stringify(data))
+const generateCtx = (chatID: Number, url: URL) => {
+  return `https://${url.host}/watch?ctx=${chatID}`
+}
+
+const pre = (text: string) => `\`${text}\``
+
+export default async (req: Request) => {
+  const data = await req.json()
+  
+  const chatID = data.message.chat.id
+  const ctx = generateCtx(chatID, new URL(req.url))
+  const message = sendMessageArg(chatID, `Hook: ${pre(ctx)}`)
+  message.set("parse_mode", "MarkdownV2")
+  
+  sendMessage(message)
+  
+  return new Response("mau")
+}
