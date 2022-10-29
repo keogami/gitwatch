54f27e26e4fe01b7ed048d0df85e83bb7702a005
keogami <41939011+keogami@users.noreply.github.com>
2022-10-29 17:45:42 +0530

    checking env variable

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,1 +1,6 @@
-export default async () => new Response("hello, world!")
+const data = {
+  myVar: Deno.env.get("MY_VAR"),
+  url: Deno.env.get("DEPLOY_URL"),
+}
+
+export default async (_req: Request) => new Response(JSON.stringify(data))
