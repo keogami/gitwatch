01c44b1fcfb7439310acf13b7f55aa3511518f6e
keogami <41939011+keogami@users.noreply.github.com>
2022-10-30 00:46:27 +0530

    moved the reusable functions to commons.ts

---
+++ netlify/edge-functions/commons.ts
@@ -0,0 +1,25 @@
+const TELEGRAM_TOKEN = Deno.env.get("TELEGRAM_TOKEN")
+const TELEGRAM_URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`
+const methodUrl = (name: String) => `${TELEGRAM_URL}/${name}`
+
+export const call = (method: string) => (body: FormData) => fetch(
+  methodUrl(method), {
+    method: "POST",
+    body: body,
+  }
+)
+
+export const sendMessage = call("sendMessage")
+
+export const sendMessageArg = (chatID: Number, text: string) => {
+  const fd = new FormData()
+  fd.set("chat_id", chatID.toString())
+  fd.set("text", text)
+  return fd
+}
+
+export const generateCtx = (chatID: Number, url: URL) => {
+  return `https://${url.host}/watch?ctx=${chatID}`
+}
+
+export const pre = (text: string) => `\`${text}\``
