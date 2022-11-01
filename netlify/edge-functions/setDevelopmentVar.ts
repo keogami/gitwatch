eebb2f3bb47db84fad82a5018bc0c3503a443a28
keogami <41939011+keogami@users.noreply.github.com>
2022-11-01 22:29:43 +0530

    fix: removed the console log

--- netlify/edge-functions/setDevelopmentVar.ts
+++ netlify/edge-functions/setDevelopmentVar.ts
@@ -3,7 +3,6 @@ import devvar, { DevelopmentVar, isDevelopment } from "./ts/development.ts"
 let hasBeenSet = false
 
 export default async (req: Request) => {
-	console.table({dev: isDevelopment(), set: hasBeenSet})
 	if (!isDevelopment() || hasBeenSet) {
 		return new Response("Bad Kitty >:( (forbidden)", {
 			status: 403,
