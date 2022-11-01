0cd2dc9445b2eb9b8cfe99001e18054f45d5d6a0
keogami <41939011+keogami@users.noreply.github.com>
2022-11-02 00:21:38 +0530

    fix: default stringer now only uses the all present objects

--- netlify/edge-functions/ts/events.ts
+++ netlify/edge-functions/ts/events.ts
@@ -2,12 +2,10 @@ import { PushEvent } from "https://cdn.skypack.dev/@octokit/webhooks-types?dts"
 import { pre } from "./commons.ts"
 
 const defaultStringer = ({
-  hook: { events },
   repository: { full_name },
   sender: { login },
 }: PushEvent) =>
 `repo: ${pre(full_name)}
-events: ${events.join(", ")}
 sender: @${login}`
 
 export default (event: string, payload: Object) => {
