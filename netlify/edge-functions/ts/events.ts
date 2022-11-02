3aceb18069da7482a510d73dc2dc3d81b5a8bb54
keogami <41939011+keogami@users.noreply.github.com>
2022-11-02 17:11:58 +0530

    fmt: changed formatting

--- netlify/edge-functions/ts/events.ts
+++ netlify/edge-functions/ts/events.ts
@@ -2,27 +2,27 @@ import { PushEvent } from "https://cdn.skypack.dev/@octokit/webhooks-types?dts"
 import { pre, escape } from "./commons.ts"
 
 const defaultStringer = ({
-  repository: { full_name },
-  sender: { login },
+	repository: { full_name },
+	sender: { login },
 }: PushEvent) =>
-`repo: ${pre(full_name)}
+	`repo: ${pre(full_name)}
 sender: @${login}`
 
 const pushStringer = (payload: PushEvent) => {
 	const { repository, head_commit: head, sender } = payload
 	const ref = (payload.ref as string).split("/")[2] // just the name
 	const action = (payload.created === true) ? "created"
-							 : (payload.deleted === true) ? "deleted"
-							 : (payload.forced === true)  ? "forced pushed to"
-	             : "pushed to"
+		: (payload.deleted === true) ? "deleted"
+			: (payload.forced === true) ? "forced pushed to"
+				: "pushed to"
 	const target = `${escape(repository.full_name)}\\:${ref}`
-  const summary =
-`[@${sender.login}](${sender.html_url}) ${action}
+	const summary =
+		`[@${sender.login}](${sender.html_url}) ${action}
 ${pre(target)}`
 	const commit =
-`\\([${head.id.slice(0, 7)}](${head.url})\\)
+		`\\([${head.id.slice(0, 7)}](${head.url})\\)
 ${head.message}`
-	
+
 	return summary + "\n\n" + commit
 }
 
@@ -33,10 +33,10 @@ const stringerMap = new Map([
 export default (event: string, payload: Object) => {
 	const s = () => `type: ${event}
 ${defaultStringer(payload as PushEvent)}`
-	
+
 	const stringer = stringerMap.get(event) ?? s
-	
+
 	const response = stringer(payload)
-	
+
 	return response
 }
