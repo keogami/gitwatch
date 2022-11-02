16be21f2f8db115a2ef15709f1fcb59692fa669d
keogami <41939011+keogami@users.noreply.github.com>
2022-11-02 17:40:51 +0530

    fix: the bot now only sends hook when it receives a command

--- netlify/edge-functions/telegram.ts
+++ netlify/edge-functions/telegram.ts
@@ -2,13 +2,16 @@ import { generateCtx, pre, sendMessage, sendMessageArg } from "./ts/commons.ts"
 
 export default async (req: Request) => {
   const data = await req.json()
+  
+  if (data?.message?.entities?.[0]?.type === "bot_command") {
+    // currently only handles /gitwatch so im not gonna bother creating a command MUXer
+    const chatID = data.message.chat.id
+    const ctx = generateCtx(chatID, new URL(req.url))
+    const message = sendMessageArg(chatID, `Hook: ${pre(ctx)}`)
+    message.set("parse_mode", "MarkdownV2")
 
-  const chatID = data.message.chat.id
-  const ctx = generateCtx(chatID, new URL(req.url))
-  const message = sendMessageArg(chatID, `Hook: ${pre(ctx)}`)
-  message.set("parse_mode", "MarkdownV2")
-
-  sendMessage(message)
+    sendMessage(message)
+  }
 
   return new Response("mau")
 }
