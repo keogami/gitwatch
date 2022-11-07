cedd9201d75ab6395d8df288dca58effa704ad92
keogami <41939011+keogami@users.noreply.github.com>
2022-11-07 21:26:03 +0530

    Revert "fix: the bot now only sends hook when it receives a command"
    This reverts commit 16be21f2f8db115a2ef15709f1fcb59692fa669d.
    Due to some errors, a faulty experiment was commited to main branch. This reverts back to a working state.

--- netlify/edge-functions/telegram.ts
+++ netlify/edge-functions/telegram.ts
@@ -3,7 +3,6 @@ import { Context } from "https://edge.netlify.com"
 
 export default async (req: Request, context: Context) => {
   const data = await req.json()
-  
   if (data?.message?.entities?.[0]?.type === "bot_command") {
     // currently only handles /gitwatch so im not gonna bother creating a command MUXer
     const chatID = data.message.chat.id
