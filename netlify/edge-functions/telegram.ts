3d09ddfa435f2fb73d5923e247ab1b980aa256ed
keogami <41939011+keogami@users.noreply.github.com>
2022-11-15 02:36:16 +0530

    store oauth info in redis and only send ouath context when no session is active

--- netlify/edge-functions/telegram.ts
+++ netlify/edge-functions/telegram.ts
@@ -1,17 +1,71 @@
 import { Bot, webhookCallback } from "https://deno.land/x/grammy/mod.ts"
+import { toHashString } from "https://deno.land/std/crypto/mod.ts" 
 import { getSiteHost, TELEGRAM_TOKEN } from "./ts/commons.ts"
+import redis, { scoped } from "./ts/redis.ts"
 
 if (typeof TELEGRAM_TOKEN === "undefined") {
   throw new Error("telegram token not found")
 }
 
+const oauthSessionCtxMapKey = scoped("oauth")
+
+interface OauthData {
+  uid: string, // id of the user
+  cid: string, // id of the chat
+}
+
+const oauthSessions = {
+  async generateContext(uid: string): Promise<string> {
+    const data = new TextEncoder().encode(uid)
+    return toHashString(await crypto.subtle.digest("sha-1", data), "hex")
+  },
+
+  async has(uid: string): Promise<boolean> {
+    const ctx = await this.generateContext(uid)
+    const res = await redis.hexists(oauthSessionCtxMapKey, ctx)
+    return res === 1
+  },
+
+  async create(data: OauthData): Promise<string> {
+    const ctx = await this.generateContext(data.uid)
+    const res = await redis.hset(oauthSessionCtxMapKey, ctx, JSON.stringify(data))
+
+    if (res !== 1) {
+      throw new Error("Redis: couldn't create oauth session")
+    }
+
+    return ctx
+  },
+
+  async retreive(ctx: string): Promise<OauthData | null> {
+    const data = await redis.hget(oauthSessionCtxMapKey, ctx)
+
+    if (data === null) return null
+    return JSON.parse(data) as OauthData
+  }
+}
+
 const bot = new Bot(TELEGRAM_TOKEN)
 
-bot.command("gitwatch", ctx => {
-  const id = ctx.chat.id
-  const host = getSiteHost()
+bot.command("gitwatch", async ctx => {
+  const uid = ctx.msg.from?.id?.toString()
+  const cid = ctx.chat.id.toString()
+
+  if (typeof uid === "undefined") {
+    ctx.reply("gitwatch should only be called by users")
+    return
+  }
+
+  if (await oauthSessions.has(uid.toString())) {
+    const data = await oauthSessions.retreive(await oauthSessions.generateContext(uid))
+    ctx.reply(`${data?.cid} found`)
+    ctx.reply("Please authenticate with the previous menu")
+    return
+  }
   
-  ctx.reply(`Hook: <pre>https://${host}/watch?ctx=${id}</pre>`, {
+  const oauthCtx = await oauthSessions.create({ cid, uid })
+
+  ctx.reply(`oauth: <pre>${oauthCtx}</pre>`, {
     parse_mode: "HTML"
   })
 })
