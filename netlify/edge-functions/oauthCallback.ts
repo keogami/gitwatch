6313497d63162d7092f390c176a65689e1c5c04f
keogami <41939011+keogami@users.noreply.github.com>
2022-11-18 03:07:50 +0530

    refactor: move github oauth code to a separate file

--- netlify/edge-functions/oauthCallback.ts
+++ netlify/edge-functions/oauthCallback.ts
@@ -1,38 +1,9 @@
 import bot from "./ts/bot.ts"
+import { exchangeCode } from "./ts/github.ts"
 import { oauthSessions } from "./ts/oauth.ts"
 import { RedisMap } from "./ts/redis.ts"
 
-const GitHubClientID = Deno.env.get("GITHUB_CLIENT_ID")
-const GitHubClientSecret = Deno.env.get("GITHUB_CLIENT_SECRET")
-
-if (typeof GitHubClientID === "undefined" || typeof GitHubClientSecret === "undefined") {
-	throw new Error("GitHub credentials not found in the environment")
-}
-
 const tokenStore = new RedisMap("token")
-
-const exchangeCode = async (code: string): Promise<string> => {
-	const body = new FormData()
-	body.set("client_id", GitHubClientID)
-	body.set("client_secret", GitHubClientSecret)
-	body.set("code", code)
-
-	const req = new Request("https://github.com/login/oauth/access_token", {
-		method: "POST",
-		headers: [
-			["Accept", "application/json"]
-		],
-		body
-	})
-	
-	const res = await (await fetch(req)).json()
-	if (res?.access_token === null || typeof res?.access_token === "undefined") {
-		console.error(res)
-		throw new Error("the access_token was not found in the response from github")
-	}
-	
-	return res.access_token
-}
 
 export default async (req: Request) => {
 	const params = new URL(req.url).searchParams
--- netlify/edge-functions/ts/menus/oauth.ts
+++ netlify/edge-functions/ts/menus/oauth.ts
@@ -1,24 +1,6 @@
 import { Menu } from "https://deno.land/x/grammy_menu@v1.1.2/menu.ts"
-import { getSiteHost } from "../commons.ts"
+import { generateOauthURL } from "../github.ts"
 import { generateContext, oauthSessions } from "../oauth.ts"
-
-const GitHubAuthURL = "https://github.com/login/oauth/authorize"
-const GitHubClientID = Deno.env.get("GITHUB_CLIENT_ID")
-const GitHubScopes = ["write:repo_hook", "admin:org_hook"].join(" ")
-if (typeof GitHubClientID === "undefined") {
-  throw new Error("Github client id was not found in the env")
-}
-
-const generateOauthURL = (ctx: string, state: string): URL => {
-  const GitHubCallbackPath = `https://${getSiteHost()}/oauthCallback`
-  const url = new URL(GitHubAuthURL)
-  url.searchParams.set("client_id", GitHubClientID)
-  url.searchParams.set("redirect_uri", GitHubCallbackPath)
-  url.searchParams.set("scope", GitHubScopes)
-  url.searchParams.set("state", `${ctx}:${state}`)
-
-  return url
-}
 
 export const oauthMenu = new Menu("oauth") 
 
