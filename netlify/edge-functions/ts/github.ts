337463f13c6036215bc2febd6c02a34ed9f80e1e
keogami <41939011+keogami@users.noreply.github.com>
2022-11-18 03:08:45 +0530

    n00b: actually track the new file for fuck's sake

---
+++ netlify/edge-functions/ts/github.ts
@@ -0,0 +1,44 @@
+import { getSiteHost } from "./commons.ts"
+
+const GitHubClientSecret = Deno.env.get("GITHUB_CLIENT_SECRET")
+const GitHubClientID = Deno.env.get("GITHUB_CLIENT_ID")
+const GitHubAuthURL = "https://github.com/login/oauth/authorize"
+const GitHubScopes = ["write:repo_hook", "admin:org_hook"].join(" ")
+
+if (typeof GitHubClientID === "undefined" || typeof GitHubClientSecret === "undefined") {
+	throw new Error("GitHub credentials not found in the environment")
+}
+
+export const exchangeCode = async (code: string): Promise<string> => {
+	const body = new FormData()
+	body.set("client_id", GitHubClientID)
+	body.set("client_secret", GitHubClientSecret)
+	body.set("code", code)
+
+	const req = new Request("https://github.com/login/oauth/access_token", {
+		method: "POST",
+		headers: [
+			["Accept", "application/json"]
+		],
+		body
+	})
+	
+	const res = await (await fetch(req)).json()
+	if (res?.access_token === null || typeof res?.access_token === "undefined") {
+		console.error(res)
+		throw new Error("the access_token was not found in the response from github")
+	}
+	
+	return res.access_token
+}
+
+export const generateOauthURL = (ctx: string, state: string): URL => {
+  const GitHubCallbackPath = `https://${getSiteHost()}/oauthCallback`
+  const url = new URL(GitHubAuthURL)
+  url.searchParams.set("client_id", GitHubClientID)
+  url.searchParams.set("redirect_uri", GitHubCallbackPath)
+  url.searchParams.set("scope", GitHubScopes)
+  url.searchParams.set("state", `${ctx}:${state}`)
+
+  return url
+}
