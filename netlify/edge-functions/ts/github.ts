a7010852dcf2756af73dee0eea5dd2ea1975bcb5
keogami <41939011+keogami@users.noreply.github.com>
2022-11-18 23:03:17 +0530

    breaking: add the user scope to the oauth token

--- netlify/edge-functions/ts/github.ts
+++ netlify/edge-functions/ts/github.ts
@@ -4,7 +4,7 @@ import { RedisMap } from "./redis.ts"
 const GitHubClientSecret = Deno.env.get("GITHUB_CLIENT_SECRET")
 const GitHubClientID = Deno.env.get("GITHUB_CLIENT_ID")
 const GitHubAuthURL = "https://github.com/login/oauth/authorize"
-const GitHubScopes = ["write:repo_hook", "admin:org_hook"].join(" ")
+const GitHubScopes = ["write:repo_hook", "admin:org_hook", "user"].join(" ")
 
 if (typeof GitHubClientID === "undefined" || typeof GitHubClientSecret === "undefined") {
 	throw new Error("GitHub credentials not found in the environment")
