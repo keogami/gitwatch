cd069ffcba131b1894f595ed2aa18e057e34c576
keogami <41939011+keogami@users.noreply.github.com>
2022-12-06 20:01:26 +0530

    feat: show a config menu when a watch item is selected

--- netlify/edge-functions/ts/menus/gitwatchlist.ts
+++ netlify/edge-functions/ts/menus/gitwatchlist.ts
@@ -1,5 +1,30 @@
 import { Menu } from "https://deno.land/x/grammy_menu@v1.1.2/menu.ts"
 import { webhookContextListStore, webhookContextStore } from "../github.ts"
+import { translate } from "https://deno.land/x/base@2.0.4/mod.ts"
+
+const hex = "0123456789abcedf"
+const base254 = String.fromCharCode(
+  ...(function* () {
+    for (let i = 0; i < 256; i++) {
+      if (i === "/".charCodeAt(0)) continue
+      yield i
+    }
+  }()),
+)
+
+const packHook = (str: string): string => {
+  return translate(str, hex, base254)
+}
+
+const unpackHook = (str: string): string => {
+  return translate(str, base254, hex)
+}
+
+const configMenu = new Menu("config").dynamic((ctx, range) => {
+  range.text({
+    text: "delete", payload: ctx.match as string
+  }, ctx => ctx.reply(unpackHook(ctx.match as string)))
+})
 
 export const watchlistMenu = new Menu("watchlist").dynamic(
   async (ctx, range) => {
@@ -10,15 +35,24 @@ export const watchlistMenu = new Menu("watchlist").dynamic(
       return
     }
     console.log(list)
-    const hydrated = await Promise.all(list.map((it) =>
-      webhookContextStore.get(it)
-    ))
-    hydrated.forEach((it) => {
+    const hydrated = await Promise.all(
+      list.map((it) =>
+        webhookContextStore.get(it).then((
+          val,
+        ): [string, typeof val] => [it, val])
+      ),
+    )
+
+    hydrated.forEach(([hook, it]) => {
       if (it === null) return
 
       const value = ("org" in it) ? it.org : `${it.owner}/${it.name}`
 
-      range.text(value, (ctx) => ctx.reply("peru nyaa~")).row()
+      range.submenu({
+        text: value,
+        payload: packHook(hook),
+      }, "config").row()
     })
   },
 )
+watchlistMenu.register(configMenu)
